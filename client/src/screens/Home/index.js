2628b451e25b8824c879cf90ec9949b541b6ef5d
Bruno Batalha <brunobatalha37@gmail.com>
2020-03-21 23:16:56 -0400

    responsividade da interface cadastro e entrar

--- client/src/screens/Home/index.js
+++ client/src/screens/Home/index.js
@@ -5,13 +5,15 @@ import './style.css';
 
 export default function Home() {
     return (
-        <div className='container-index'>
-            <div>
-                <Logo></Logo>
+        <div className='container-main'>
+            <div className='container-index'>
+                <div>
+                    <Logo></Logo>
+                </div>
+                <main>
+                    <Form></Form>
+                </main>
             </div>
-            <main>
-                <Form></Form>
-            </main>
         </div>
     );
 }
